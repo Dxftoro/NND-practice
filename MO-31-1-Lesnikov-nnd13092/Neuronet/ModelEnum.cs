53fef08c90fe27d7ed4a65404302e9c5dc7899dd
Ftoro <lesnik.antonio@gmail.com>
2025-09-21 01:01:27 +0300

    add neuron class and some enums

---
+++ MO-31-1-Lesnikov-nnd13092/Neuronet/ModelEnum.cs
@@ -0,0 +1,22 @@
+﻿namespace MO_31_1_Lesnikov_nnd13092.Neuronet
+{
+    enum MemoryMode
+    {
+        GET,
+        SET,
+        INIT
+    }
+
+    enum NeuronType
+    {
+        HIDDEN,
+        OUTPUT
+    }
+
+    enum NetworkMode
+    {
+        TRAIN,      // learning mode
+        TEST,       // testing mode
+        DEMO        // recognising mode
+    }
+}
