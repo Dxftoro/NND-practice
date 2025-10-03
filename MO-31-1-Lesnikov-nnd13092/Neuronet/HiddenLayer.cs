d73c3902df657eb288af58c31e0a7e1b9e546a3d
Ftoro <lesnik.antonio@gmail.com>
2025-10-03 05:02:35 +0300

    add layer class

---
+++ MO-31-1-Lesnikov-nnd13092/Neuronet/HiddenLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MO_31_1_Lesnikov_nnd13092.Neuronet
+{
+    class HiddenLayer
+    {
+    }
+}
